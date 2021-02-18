43f3eed68b1505263a9582907ddd911a271bbbb7
Ahos92 <ahosism@gmail.com>
2021-02-18 11:40:33 +0900

    2 / 18 파일명 중복, 예외시 파일 삭제 처리

--- KFLIX/src/main/java/com/kflix/movie/controller/MovieController.java
+++ KFLIX/src/main/java/com/kflix/movie/controller/MovieController.java
@@ -44,7 +44,7 @@ public class MovieController {
 	GenreService gr_service;
 	
 	@Inject
-	FileUploadService upload_serevice;
+	FileUploadService upload_service;
 	
 	@Inject
 	Movie movie;
@@ -99,25 +99,27 @@ public class MovieController {
 	public String addMovie(Model model, Movie movie, 
 				MultipartFile poster, MultipartFile teaser, MultipartFile video) {
 		
-		String msg = "";
+		String msg = "파일 업로드에 실패하였습니다. 다시 한번 확인해주세요";
 		//날짜 체크 , 파일 유효성 체크
-		if(mv_service.checkDate(movie) && upload_serevice.checkExtAll(poster, teaser, video)) {
+		if(mv_service.checkDate(movie) 
+				&& upload_service.checkExtAll(poster, teaser, video)
+				&& upload_service.checkOverLaps(poster, teaser, video)) {
 
-			upload_serevice.setPathNames(poster, teaser, video, movie);
+			upload_service.setPathNames(poster, teaser, video, movie);
 			
-			// DB 처리
-			int result = mv_service.insertNewMovie(movie);
+			// 파일 업로드
+			boolean upload_result = upload_service.upload(poster, teaser, video, movie);
 
-			if (result > 0 ) {
-				// 파일 업로드
-				upload_serevice.upload(poster, teaser, video, movie);
+			// DB 처리
+			int db_result = mv_service.insertNewMovie(movie);
+
+			if (db_result > 0 && upload_result) {
 				msg = "등록하였습니다!";
 				
 			} else {
-				msg = "등록에 실패하였습니다.";
+				upload_service.fileDelete(poster, teaser, video);
 				
 			}
-			
 		}
 		
 		model.addAttribute("msg", msg);
@@ -150,15 +152,42 @@ public class MovieController {
 	public String update(Model model, Movie movie,
 						MultipartFile poster, MultipartFile teaser, MultipartFile video) {
 		
-		String msg = "";
+//		String msg = "파일 업로드에 실패하였습니다. 다시 한번 확인해주세요";
+//		//날짜 체크 , 파일 유효성 체크
+//		if(mv_service.checkDate(movie) 
+//				&& upload_service.checkExtAll(poster, teaser, video)
+//				&& upload_service.checkOverLaps(poster, teaser, video)) {
+//
+//			upload_service.setPathNames(poster, teaser, video, movie);
+//			
+//			// 파일 업로드
+//			boolean upload_result = upload_service.upload(poster, teaser, video, movie);
+//
+//			// DB 처리
+//			int db_result = mv_service.insertNewMovie(movie);
+//
+//			if (db_result > 0 && upload_result) {
+//				msg = "등록하였습니다!";
+//				
+//			} else {
+//				upload_service.fileDelete(poster, teaser, video);
+//				
+//			}
+//		}
+//		
+//		model.addAttribute("msg", msg);
+//		return "movie/result";
+		
+		String msg = "파일 업로드에 실패하였습니다. 다시 한번 확인해주세요";
 		// 날짜 체크
 		if (mv_service.checkDate(movie)) {
-			upload_serevice.setPathNames(poster, teaser, video, movie);
+			
+			upload_service.setPathNames(poster, teaser, video, movie);
 			int result = mv_service.updateMovie(movie);
 			
 			if (result > 0) {
 				// 파일 업로드
-				upload_serevice.upload(poster, teaser, video, movie);
+				upload_service.upload(poster, teaser, video, movie);
 				msg = "수정 되었습니다.";
 				
 			} else {
--- KFLIX/src/main/java/com/kflix/util/fileupload/FileUploadServiceImpl.java
+++ KFLIX/src/main/java/com/kflix/util/fileupload/FileUploadServiceImpl.java
@@ -1,10 +1,11 @@
 package com.kflix.util.fileupload;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
-
+import java.util.UUID;
 
 import org.apache.tika.Tika;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,7 @@ import lombok.extern.log4j.Log4j;
 @Service
 @Log4j
 public class FileUploadServiceImpl implements FileUploadService {
-	
+
 
 	// jsp 요청
 	private static final String[] PREFIX_URI = {
@@ -32,6 +33,16 @@ public class FileUploadServiceImpl implements FileUploadService {
 	private static int VIDEO = 2;
 
 	Tika tika = new Tika();
+
+	String class_path = this.getClass().getResource("/").getPath();
+	String local_path = class_path.substring(1, class_path.indexOf("SPRING/"));
+
+	// 저장할 주소
+	String[] SAVE_PATH = {
+			local_path + "k-flix/KFLIX/src/main/webapp/resources/imgs/movie/poster/",
+			local_path + "k-flix/KFLIX/src/main/webapp/resources/videos/teaser/",
+			local_path + "k-flix/KFLIX/src/main/webapp/resources/videos/full/"
+	};
 
 
 	@Override
@@ -53,9 +64,6 @@ public class FileUploadServiceImpl implements FileUploadService {
 	// 확장자 유효성 검사
 	@Override
 	public boolean checkExtAll(MultipartFile poster, MultipartFile teaser, MultipartFile video) {
-		String class_path = this.getClass().getResource("/").getPath();
-		String local_path = class_path.substring(1, class_path.indexOf("SPRING/"));
-		log.info(local_path);
 		if (poster == null && teaser == null && video == null) return false;
 
 		try {
@@ -112,52 +120,58 @@ public class FileUploadServiceImpl implements FileUploadService {
 		return false;
 	}
 
-	// 현재 시간을 기준으로 파일 이름 생성
-	private String getSaveFileName(String ext_name) {
-		String fileName = "";
+	
+	private boolean checkOverlap(MultipartFile file, int i) {
+		File check_file = new File(SAVE_PATH[i]);
 
-		Calendar calendar = Calendar.getInstance();
-		fileName += calendar.get(Calendar.YEAR);
-		fileName += calendar.get(Calendar.MONTH);
-		fileName += calendar.get(Calendar.DATE);
-		fileName += calendar.get(Calendar.HOUR);
-		fileName += calendar.get(Calendar.MINUTE);
-		fileName += calendar.get(Calendar.SECOND);
-		fileName += calendar.get(Calendar.MILLISECOND);
-		fileName += ext_name;
+		if (!check_file.isDirectory()) {
 
-		return fileName;
+			if(check_file.mkdir()) {
+				log.info("디렉토리 만들기 성공");
+
+			} else {
+				log.info("디렉토리 만들기 실패");
+				return false;
+			}
+
+		}
+		
+		String origin_name = file.getOriginalFilename();
+		
+		check_file = new File(SAVE_PATH[i] + origin_name);
+		
+		// 파일 존재 여부 중복처리
+		if (!check_file.exists()) {
+			log.info(origin_name + " - 중복 되지 않음");
+			return true;
+
+		}  else {
+
+			log.info(origin_name + " - 중복 파일");
+			
+		}
+		return false;
 	}
 
-	private boolean writeFile(MultipartFile file, int i) throws IOException{
-		//				ext_name = originFile_name.substring(originFile_name.lastIndexOf("."), originFile_name.length());
-		//				saveFileName = getSaveFileName(ext_name);			
-		//
-		//				log.info("겟 오리진 : "+originFile_name);
-		//				log.info("저장될 이름 : "+saveFileName);
 
-		String class_path = this.getClass().getResource("/").getPath();
-		String local_path = class_path.substring(1, class_path.indexOf("SPRING/"));
-		
-		// 저장할 주소
-		String[] SAVE_PATH = {
-				local_path + "k-flix/KFLIX/src/main/webapp/resources/imgs/movie/poster/",
-				local_path + "k-flix/KFLIX/src/main/webapp/resources/videos/teaser/",
-				local_path + "k-flix/KFLIX/src/main/webapp/resources/videos/full/"
-		};
-		
+	private void writeFile(MultipartFile file, int i) throws IOException{
 		if(checkExt(file)) {
 			String origin_name = file.getOriginalFilename();
+			String upload_path = SAVE_PATH[i] + origin_name;
+			//			String ext_name = origin_name.substring(origin_name.lastIndexOf("."), origin_name.length());
+			//			
+			//			UUID uid = UUID.randomUUID();
+
 			byte[] data = file.getBytes();
-			FileOutputStream fos = new FileOutputStream(SAVE_PATH[i] + origin_name);
+			FileOutputStream fos = new FileOutputStream(upload_path);
 			fos.write(data);
 			fos.close();
 			log.info(origin_name + " 업로드 성공");			
-			return true;
 
-		} else {
-			return false;
+		}  else {
+			log.info("수정하지 않은 파일");
 		}
+
 	};
 
 	private void setPathName(MultipartFile file, int num, Movie movie){	
@@ -195,6 +209,26 @@ public class FileUploadServiceImpl implements FileUploadService {
 
 	}
 
+	private boolean delete(MultipartFile file, int num) {
+		String origin_name = file.getOriginalFilename();
+		File check = new File(SAVE_PATH[num] + origin_name);
+		
+		if (check.exists()) {
+			
+			if(check.delete()) {
+				log.info(origin_name + "  - 삭제 성공");
+			} else {
+				log.info(origin_name + " - 삭제 실패");
+			}
+			
+		} else {
+			log.warn(origin_name + " - File not Found");
+		}
+		
+		
+		return true;
+	}
+	
 
 	@Override
 	public boolean upload(MultipartFile poster, MultipartFile teaser, MultipartFile video, Movie movie) {
@@ -204,7 +238,7 @@ public class FileUploadServiceImpl implements FileUploadService {
 			writeFile(poster, POSTER);
 			writeFile(teaser, TEASER);
 			writeFile(video, VIDEO);
-			
+
 			return true;
 		} catch (IOException e) {
 			log.info(e.getMessage());
@@ -222,5 +256,24 @@ public class FileUploadServiceImpl implements FileUploadService {
 
 	}
 
+	@Override
+	public boolean checkOverLaps(MultipartFile poster, MultipartFile teaser, MultipartFile video) {
+		boolean poster_ck = checkOverlap(poster, POSTER);
+		boolean teaser_ck = checkOverlap(teaser, TEASER);
+		boolean video_ck = checkOverlap(video, VIDEO);
+		
+		return poster_ck && teaser_ck && video_ck;
+	}
+
+
+
+	@Override
+	public boolean fileDelete(MultipartFile poster, MultipartFile teaser, MultipartFile video) {
+		delete(poster, POSTER);
+		delete(teaser, TEASER);
+		delete(video, VIDEO);
+		
+		return true;
+	}
 
 }
