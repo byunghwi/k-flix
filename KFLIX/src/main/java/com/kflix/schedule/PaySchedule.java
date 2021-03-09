9f1b503d43f0dfae784f2798c72e83675214e826
byunghwi <gpdldy91@gmail.com>
2021-03-09 15:36:42 +0900

    이용권 해지 완료

--- KFLIX/src/main/java/com/kflix/schedule/PaySchedule.java
+++ KFLIX/src/main/java/com/kflix/schedule/PaySchedule.java
@@ -25,6 +25,7 @@ public class PaySchedule {
 	@Inject
 	private KakaoPay kakaoPayService;
 	
+	// 정기결제 대상 회원 추출 후 정기결제 진행
 	@Scheduled(cron = "0 0 0 * * *")
 	public void payScheduling() {
 		Member member = null;
@@ -46,6 +47,29 @@ public class PaySchedule {
 			// 정기 결제 후 회원 정보 업데이트 ( 이용권번호, 만료일, sid, 정기결제 해지일 )
 			memberService.updatePayMember(member);
 		}
+	}
+	
+	// 정기결제 해지 회원 추출 후 만료일 시점에 이용권 해지
+	@Scheduled(cron = "0 0 0 * * *")
+	public void removePaySchduling() {
+		Member member = null;
+		Ticket ticket = null;
+		System.out.println("[removePaySchedule] 진입");
+		
+		// 정기결제 해지 회원 목록 추출 - 00:00 자정 기준 Member테이블 pay_exp_date(만료일)과 현재일이 일치 + sid 없음 + pay_remove_dt(해지일)존재 
+		List<Member> delPayMemList = memberService.scheduleDelPayMemList();
+		
+		for (int i = 0; i < delPayMemList.size(); i++) {
+			System.out.println("[removePaySchedule] member > " + delPayMemList.get(i));
+			member = delPayMemList.get(i);
+			
+			member.setPay_exp_date(null);  // 만료일
+			member.setPay_remove_dt(null); // 해지일
+			member.setPay_sid(null);	   // 정기결제 id	
+			member.setTicket_id(0);		   // 이용권 id
 
+			//해지 회원 정보 업데이트 ( 이용권번호, 만료일, sid, 정기결제 해지일 모두 null처리 )
+			memberService.updatePayMember(member);
+		}
 	}
 }
